cf59e41d2e9cda2569c4ff1a633811ff04f00e8b
liquidz <liquidz.uo@gmail.com>
2013-08-20 21:25:21 +0900

    add files

---
+++ content/core.js
@@ -0,0 +1,32 @@
+(function(window, undefined){
+    var self = {};
+
+    self.title = "Selenium IDE: Pretty Report";
+
+    self.foo = function(){
+        var title = window.editor.app.getTestCase().getTitle();
+
+        alert(
+            IDE_UTIL.getTestCases().map(IDE_UTIL.parseTestCase).map(function(testcase){
+                if(testcase['type'] === 'comment'){
+                    return(testcase['value']);
+                } else {
+                    var text = testcase['command'] + ":" + testcase['target'] + ":" + testcase['value'] + ":" + testcase['result'];
+                    return(text);
+                }
+            }).join("\n")
+        );
+    };
+
+    self.exportTestCaseResults = function(){
+        var file     = IDE_UTIL.openFileDialog("title")
+          , contents = ""
+          ;
+
+        IDE_UTIL.writeFile(file, contents);
+    };
+
+    if(!window.SIPR){
+        window.SIPR = self;
+    }
+}(window));
---
+++ content/ide_util.js
@@ -0,0 +1,69 @@
+(function(window, undefined){
+    var self = {};
+
+    self.writeFile = function(file, contents){
+        try {
+            var output    = FileUtils.openFileOutputStream(file)
+              , converter = FileUtils.getUnicodeConverter("UTF-8")
+              , text      = converter.ConvertFromUnicode(contents)
+              ;
+            output.write(text, text.length);
+            var fin = converter.Finish();
+            if(fin.length > 0){ output.write(fin, fin.length); }
+            output.close();
+            return true;
+        } catch(e){
+            return false;
+        }
+    };
+
+    self.openFileDialog = function(title){
+        return showFilePicker(
+                window
+              , title
+              , Components.interfaces.nsIFilePicker.modeSave
+              , Format.TEST_CASE_EXPORT_DIRECTORY_PREF
+              , function(fp){ return fp.file; }
+        );
+    };
+
+    var _parseComment = function(src){
+        if(src.match(/<!--\s*(.+?)\s*-->/)){
+            return({type: "comment", value: RegExp.$1});
+        }
+        return("err1:" + src);
+    };
+
+    var _parseCommand = function(src){
+        if(src.match(/<td>(.+?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*/)){
+            return({type: "command", command: RegExp.$1, target: RegExp.$2, value: RegExp.$3});
+        }
+        return("err2:" + src);
+    };
+
+    self.parseTestCase = function(cmd){
+        var src = getSourceForCommand(cmd)
+          , res = null
+          ;
+        if(src.indexOf("<!--") !== -1){
+            res = _parseComment(src);
+        } else {
+            res = _parseCommand(src);
+        }
+        res.result = cmd.result;
+
+        return(res);
+    };
+
+    self.getTestCases = function(){
+        var file = window.editor.app.getTestCase();
+        return(file.commands.reduce(function(res, cmd){
+            return(res.concat(cmd));
+        }, []));
+    };
+
+    if(!window.IDE_UTIL){
+        window.IDE_UTIL = self;
+    }
+
+}(window));
