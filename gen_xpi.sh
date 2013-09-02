0992f446d94057ec5d90c4df9c01bdc5b84f85e6
liquidz <liquidz.uo@gmail.com>
2013-09-02 22:31:00 +0900

    update gen_xpi.sh

--- gen_xpi.sh
+++ gen_xpi.sh
@@ -4,4 +4,5 @@ RDF="install.rdf"
 VERSION=`grep version $RDF | cut -d">" -f2 | cut -d"<" -f1 | tr -d '\n'`
 PACKAGE="selenium_ide_pretty_report-${VERSION// }-fx.xpi"
 
+rm -rf selenium_ide_pretty_report-*-fx.xpi
 zip -r $PACKAGE * -x LICENSE README.md images/ images/* $0 $PACKAGE
